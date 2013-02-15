8536764f4b918be3151cd3e71a94087072989e35
Bing Chong <bingchong@ubuntu.(none)>
2013-02-15 14:06:19 -0800

    sd

--- webservice/testLib.py
+++ webservice/testLib.py
@@ -21,7 +21,7 @@ class RestTestCase(unittest.TestCase):
     ERR_BAD_PASSWORD    = -4
     
     # Lookup the name of the server to test
-    serverToTest = "127.0.0.1:8000"
+    serverToTest = "frozen-anchorage-1167.herokuapp.com"
     if "TEST_SERVER" in os.environ:
         serverToTest = os.environ["TEST_SERVER"]
 
