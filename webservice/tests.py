1493f0e2ea322574264841f66c041e9a3c7b4baf
Bing Chong <bingchong@ubuntu.(none)>
2013-02-15 16:00:04 -0800

    sial2

--- webservice/tests.py
+++ webservice/tests.py
@@ -6,10 +6,17 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+import os, sys
+from os.path import abspath, dirname
+project_dir = abspath(dirname(dirname(__file__)))
+sys.path.insert(0, project_dir)
+os.environ['DJANGO_SETTINGS_MODULE'] = "mysite.settings"
 
+'''
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+'''
